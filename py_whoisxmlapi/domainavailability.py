e3a9e99079c4c9f891e309518e43884805f68588
EC2 Default User <ec2-user@ip-10-0-0-167.ec2.internal>
2018-01-29 17:24:20 +0000

    first version

---
+++ py_whoisxmlapi/domainavailability.py
@@ -0,0 +1,92 @@
+import requests
+import json
+import logging
+import requests
+import time
+import sys
+import os
+
+class domainAvailability:
+
+    def __init__(self, usr=None, pwd=None):
+        try:
+            
+            if usr is None:
+                raise Exception("No User name!")
+                
+            if pwd is None: 
+                raise Exception("No User password!")
+            
+            self.usr = usr
+            self.pwd = pwd
+            
+            self.url = "https://www.whoisxmlapi.com/whoisserver/WhoisService?"\
+                        "cmd=GET_DN_AVAILABILITY&domainName={}&username={}&"\
+                        "password={}&getMode=DNS_AND_WHOIS&outputFormat=JSON"
+            
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.warning(str(e) + " | " + str(exc_type) + " | " + str(fname) + " | " + str(exc_tb.tb_lineno)) 
+
+
+    def get_url(self, url=None):
+        try:
+            if url is None:
+                raise Exception("No Url!")
+            
+            r = requests.get(url, timeout=1)
+            if r.status_code == 200:
+                content = r.content.decode("utf8")
+                return content
+            else:
+                raise Exception("Error getting URL : "+ str(r.status_code))
+            
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.warning(str(e) + " | " + str(exc_type) + " | " + str(fname) + " | " + str(exc_tb.tb_lineno)) 
+            return False
+
+    def get_json_from_url(self, url=None):
+        try:
+            if url is None:
+                raise Exception("No Url!")
+            
+            content = self.get_url(url)
+            
+            if content is None or content == "" or content is False:
+                raise Exception("No content!")
+            
+            js = json.loads(content)
+            return js
+
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.warning(str(e) + " | " + str(exc_type) + " | " + str(fname) + " | " + str(exc_tb.tb_lineno)) 
+    
+    
+    def checkDomain(self, domain):
+        try:
+            usr = self.usr
+            pwd = self.pwd
+            
+            url = self.url.format(domain, usr, pwd)
+            
+            data = self.get_json_from_url(url)
+            
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.warning(str(e) + " | " + str(exc_type) + " | " + str(fname) + " | " + str(exc_tb.tb_lineno)) 
+
+        try:
+            data = data['DomainInfo']
+            return data['domainAvailability']
+        except KeyError:
+            return "Unknown"
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.warning(str(e) + " | " + str(exc_type) + " | " + str(fname) + " | " + str(exc_tb.tb_lineno))             
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='py_whoisxmlapi',
+      version='0.1',
+      description='Python3 WhoisService data parser',
+      long_description=readme(),
+      url='https://bitbucket.org/tvdsluijs/py_whoisxmlapi',
+      author='Theo van der Sluijs',
+      author_email='theo@vandersluijs.nl',
+      license='CC BY-NC-SA 4.0',
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+        ],
+      keywords='whois domains',
+      packages=['py_whoisxmlapi'],
+      zip_safe=False)
